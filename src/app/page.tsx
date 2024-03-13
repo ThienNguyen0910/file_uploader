f0cbd0a91dfc32b4e648a9d123240dfdff1f15c2
ThienNguyen0910 <thiennguyenit0910@gmail.com>
2024-03-13 16:54:47 +0700

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+
+export default function Home() {
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignedIn>
+        <SignOutButton>
+          <Button>sign out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign in</Button>
+        </SignInButton>
+      </SignedOut>
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+      <Button onClick={() => createFile({ name: "concac" })}>button</Button>
+    </main>
+  );
+}
